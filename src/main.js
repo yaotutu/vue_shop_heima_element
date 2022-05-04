271394ef8a2ad933bf853ca7bba499c117012524
yaotutu <957810668@qq.com>
2022-05-04 22:33:17 +0800

    删除无用代码,配置好饿了么UI,router基础配置.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import VueRouter from 'vue-router'
+import Vue from 'vue';
+import { Button, Select } from 'element-ui';
+import App from './App.vue';
+import router from './router'
 
-Vue.config.productionTip = false
+Vue.use(Button)
+ 
 
 new Vue({
-  render: h => h(App),
+  el: '#app',
+  router,
+  render: h => h(App)
 }).$mount('#app')
